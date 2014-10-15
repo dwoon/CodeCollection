2ba0899e618b233b7eba1af90b9baa427de6f60c
dwoon <hxlwysh@gmail.com>
2014-10-15 20:28:03 +0800

    url decode encode

---
+++ shell/url.sh
@@ -0,0 +1,32 @@
+urlencode() {
+  arg="$1"
+  i="0"
+  while [ "$i" -lt ${#arg} ]; do
+    c=${arg:$i:1}
+    if echo "$c" | grep -q '[a-zA-Z/:_\.\-]'; then
+     echo -n "$c"
+    else
+     echo -n "%"
+     printf "%X" "'$c'"
+    fi  
+    i=$((i+1))
+  done
+}
+
+urldecode() {
+  arg="$1"
+  i="0"
+  while [ "$i" -lt ${#arg} ]; do
+    c0=${arg:$i:1}
+    if [ "x$c0" = "x%" ]; then
+     c1=${arg:$((i+1)):1}
+     c2=${arg:$((i+2)):1}
+     printf "\x$c1$c2"
+     i=$((i+3))
+    else
+     echo -n "$c0"
+     i=$((i+1))
+   fi  
+  done
+  echo ''
+}  
